a293c093b6268c33573aafef07d13fb625dfaa37
Princess Ethel <65485445+ethylene03@users.noreply.github.com>
2024-12-02 21:43:43 +0800

    add end game

--- Assets/Scripts/globalVariables.cs
+++ Assets/Scripts/globalVariables.cs
@@ -4,7 +4,7 @@ public static class globalVariables {
     public static float oppositeCarSpeed = 5.0f;
 
     public static float playerSpeed = 0f;
-    public static float maxPlayerSpeed = 0.005f;
+    public static float maxPlayerSpeed = 0.01f;
     public static float offsetValue = 0f;
     public static float currentOffset = 0f;
 }
--- Assets/Scripts/trackMove.cs
+++ Assets/Scripts/trackMove.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class trackMove : MonoBehaviour
 {
     Vector2 offset;
     public GameObject crossingObject;
-    public float spawnCrossingAt;
+    public GameObject[] landmarks;
+    private float spawnCrossingAt;
+    public float[] landmarkDistance = new float[]{ 10f, 20f };
+    private float spawnLandmarksAt;
+    private int landmarkIdx;
+    public GameObject gameOverText;
 
-    void SpawnObjectAtOffset()
+
+    void SpawnObjectAtOffset(GameObject obj)
     {
-        Vector3 spawnPosition = new Vector3(0f, 7.4f, 99f);
-        Instantiate(crossingObject, spawnPosition, Quaternion.identity);
+        Vector3 spawnPosition = new Vector3(obj.transform.localPosition.x, 7.4f, obj.transform.localPosition.z);
+        Instantiate(obj, spawnPosition, obj.transform.rotation);
     }
 
     void Start() {
         globalVariables.currentOffset = 0;
         spawnCrossingAt = Random.Range(globalVariables.currentOffset + 5f, globalVariables.currentOffset + 15f);
+        spawnLandmarksAt = Random.Range(landmarkDistance[0], landmarkDistance[1]);
+        landmarkIdx = 0;
     }
 
     void Update() {
@@ -39,11 +45,30 @@ public class trackMove : MonoBehaviour
 
             // spawn crossing
             if(globalVariables.currentOffset >= spawnCrossingAt) {
-                SpawnObjectAtOffset();
+                SpawnObjectAtOffset(crossingObject);
                 spawnCrossingAt = Random.Range(globalVariables.currentOffset + 5f, globalVariables.currentOffset + 15f);
+            }
+
+            // spawn landmarks
+            if(globalVariables.currentOffset >= spawnLandmarksAt) {
+                SpawnObjectAtOffset(landmarks[landmarkIdx]);
+                spawnLandmarksAt = Random.Range(globalVariables.currentOffset + landmarkDistance[0], globalVariables.currentOffset + landmarkDistance[1]);
+
+                if(landmarkIdx == 3) {
+                    // end game
+                    globalVariables.startGame = false;
+                    EndGame();
+                } else
+                    landmarkIdx = (landmarkIdx + 1) % 4;
             }
         } else {
             Time.timeScale = 0;
         }
+    }
+
+    void EndGame() {
+        if(gameOverText != null)
+            gameOverText.SetActive(true);
+        Time.timeScale = 0;
     }
 }
