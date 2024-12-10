e820a233d8e65332ed91e79ad250eb89485c3f99
dons <damozo@up.edu.ph>
2024-12-10 17:22:45 +0800

    update
    temporarily disabled 321, add fullscreenview script

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,15 +7,19 @@ public class SoundManager : MonoBehaviour
     // [SerializeField] Image PauseButton;
     // [SerializeField] Image ContinueButton;
 
+    // private bool muted = false;
+
     void Start(){
         AudioListener.pause = false;
     }
 
     void Update(){
         if(globalVariables.startGame){
+            // muted = true;
             AudioListener.pause = false;
         }
         else{
+            // muted = false;
             AudioListener.pause = true;
         }
     }
