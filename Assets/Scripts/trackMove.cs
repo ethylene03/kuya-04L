4c8f99c3bda8d7557928a9e0a5e0280ee5f3dd91
Jhoanna Lagumbay <91950450+jtlagumbay@users.noreply.github.com>
2024-12-11 03:57:34 +0800

    fix you lose

--- Assets/Scripts/trackMove.cs
+++ Assets/Scripts/trackMove.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Concurrent;
 using System.Net;
+using QFSW.QC;
+using TMPro;
+using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class trackMove : MonoBehaviour
 {
@@ -18,6 +24,8 @@ public class trackMove : MonoBehaviour
     private GameConstants gameConstants;
     public GameObject winText;
     public GameObject loseText;
+    private bool isWinner = false;
+    private ConcurrentQueue<string> messageQueue = new ConcurrentQueue<string>();
 
 
     void SpawnObjectAtOffset(GameObject obj)
@@ -28,10 +36,11 @@ public class trackMove : MonoBehaviour
 
     void Start() {
         Debug.Log("trackMove Start");
+        isWinner = false;
         gameConstants = new GameConstants();
         Debug.Log("gameConstants " + gameConstants);
         broadcastManager = new BroadcastManager(gameConstants.GAME_OVER_PORT);
-        broadcastManager.HandleOnReceive = EndGame;
+        broadcastManager.HandleOnReceive = broadcastReceive;
         broadcastManager.IsListening = true;
         Debug.Log("broadcastManager " + broadcastManager);
 
@@ -51,6 +60,14 @@ public class trackMove : MonoBehaviour
             return;
         }
 
+        while (messageQueue.TryDequeue(out string message))
+        {
+            Debug.Log("Dequeueing " + message);
+            // Process the dequeued message
+            EndGame(message);
+        }
+
+
         if(globalVariables.startGame) {
             Time.timeScale = 1;
 
@@ -58,7 +75,7 @@ public class trackMove : MonoBehaviour
             float offsetValue = Mathf.Clamp(globalVariables.playerSpeed, 0, globalVariables.maxPlayerSpeed);
             globalVariables.offsetValue = offsetValue;
             playerCar.currentOffset.Value += offsetValue;
-            Debug.Log("offset: " + offsetValue + ", currOffset: " + playerCar.currentOffset.Value);
+            // Debug.Log("offset: " + offsetValue + ", currOffset: " + playerCar.currentOffset.Value);
             
             MoveTrack();
 
@@ -76,11 +93,8 @@ public class trackMove : MonoBehaviour
                 // abot na sa SM
                 SpawnObjectAtOffset(landmarks[3]);
 
-                // end game
-                broadcastManager.IsListening = false;
-                broadcastManager.SendBroadcast(gameConstants.GAME_OVER_LOSE);
-  
-                EndGame(gameConstants.GAME_OVER_WIN);
+                
+                HandleWinner();
             } else if(playerCar.currentOffset.Value >= spawnLandmarksAt && landmarkIdx < 3) {
                 SpawnObjectAtOffset(landmarks[landmarkIdx]);
                 spawnLandmarksAt = Random.Range(playerCar.currentOffset.Value + landmarkDistance[0], playerCar.currentOffset.Value + landmarkDistance[1]);
@@ -92,35 +106,71 @@ public class trackMove : MonoBehaviour
         }
     }
 
+    [Command]
+    public void HandleWinner(){
+        isWinner = true;
+        broadcastManager.SendBroadcast(gameConstants.GAME_OVER_LOSE);
+        broadcastManager.SendBroadcast(gameConstants.GAME_OVER_LOSE);
+        EndGame(gameConstants.GAME_OVER_WIN);
+    }
+
+    [Command]
+    public void TestLose(){
+        EndGame(gameConstants.GAME_OVER_LOSE);
+    }
+
+    [Command]
+
+    public void TestWin(){
+        EndGame(gameConstants.GAME_OVER_WIN);
+    }
+
     public void MoveTrack() {
         offset = new Vector2(0, playerCar.currentOffset.Value);
-        Debug.Log("trackMove offset: " + offset);
+        // Debug.Log("trackMove offset: " + offset);
 
         GetComponent<Renderer> ().material.mainTextureOffset = offset;
     }
 
-    void EndGame(string receivedMessage = null, IPEndPoint endPoint = null) {
-        Debug.Log("EndGame "+ receivedMessage);
-        globalVariables.startGame = false;
-        
-        if(loseText == null || winText == null) return;
-        
-        Debug.Log("endgame 2");
-        Debug.Log("gameConstants "  + gameConstants);
-        gameConstants = new GameConstants();
-        
-        if(receivedMessage == gameConstants.GAME_OVER_WIN){
-            winText.SetActive(true);
-            loseText.SetActive(false);
-        } else if (receivedMessage == gameConstants.GAME_OVER_LOSE) {
-            winText.SetActive(false);
-            loseText.SetActive(true);
+    void broadcastReceive(string receivedMessage = null, IPEndPoint endPoint = null){
+        messageQueue.Enqueue(receivedMessage);
+    }
+
+    void EndGame(string receivedMessage = null, Boolean restart = true) {
+        try {
+            if(isWinner && receivedMessage == gameConstants.GAME_OVER_LOSE) return;
+            Debug.Log("EndGame "+ receivedMessage);
+            globalVariables.startGame = false;
+            
+            if(loseText == null || winText == null) return;
+            
+            Debug.Log("endgame 2");
+            Debug.Log("gameConstants "  + gameConstants);
+            gameConstants = new GameConstants();
+            Debug.Log("gameConstants 2 "  + gameConstants);
+
+            
+            if(receivedMessage == gameConstants.GAME_OVER_WIN){
+                Debug.Log("Win");
+                // winText.SetActive(true);
+                // loseText.SetActive(false);
+                TMP_Text winTMP_Text= winText.GetComponent<TMP_Text>();
+                winTMP_Text.text = "You Win";
+            } else if (receivedMessage == gameConstants.GAME_OVER_LOSE) {
+                Debug.Log("Lose");
+                // winText.SetActive(false);
+                // loseText.SetActive(true);
+                TMP_Text winTMP_Text= winText.GetComponent<TMP_Text>();
+                winTMP_Text.text = "You Lose";
+            }
+            
+            
+            Time.timeScale = 0;
+            new WaitForSecondsRealtime(5f);
+            SceneManager.LoadScene("players-board");
+        } catch (SystemException e){
+            Debug.Log(e);
         }
-        
-        
-        Time.timeScale = 0;
-        new WaitForSecondsRealtime(5f);
-        SceneManager.LoadScene("players-board");
     }
 
     void AdjustOpponentCars(){
@@ -136,13 +186,13 @@ public class trackMove : MonoBehaviour
             }
         }
 
-        Debug.Log("ownerOffset " + ownerOffset);
+        // Debug.Log("ownerOffset " + ownerOffset);
 
         foreach (var car in opponentCars)
         {
             carControl carObj = car.GetComponent<carControl>();
-            Debug.Log("CarObj id: " + carObj.OwnerClientId);
-            Debug.Log("position: " + car.transform.position);
+            // Debug.Log("CarObj id: " + carObj.OwnerClientId);
+            // Debug.Log("position: " + car.transform.position);
 
             if (!carObj.IsOwner)
             {
